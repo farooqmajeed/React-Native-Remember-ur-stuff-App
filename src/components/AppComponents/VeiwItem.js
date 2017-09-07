3def21ba1890b4da96a85f1f1cd6445673410aa5
farooqmajeed <farooq306@hotmail.com>
2017-09-07 13:14:10 +0500

    View items List

--- src/components/AppComponents/VeiwItem.js
+++ src/components/AppComponents/VeiwItem.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import { Text, StyleSheet, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import DatePicker from 'react-native-datepicker'
-import { Container, Content, Header, Left, Body, Right, Button, Icon, Title, CardItem, Item, Input, Card, H2, Label, Form, List, ListItem } from 'native-base';
+import { Container, Content, Header, Left, Body, Right, Button, Icon, Title, CardItem, Item, Input, Card, H2, Label, Form, Fab, List, ListItem } from 'native-base';
 import { AppActions } from '../../store/actions/AppActions';
+import * as firebase from '../../configs/db';
 
 class VeiwItems extends Component {
 
@@ -13,8 +14,24 @@ class VeiwItems extends Component {
         console.log("available PROps", this.props.viewItems());
     }
 
-    onButtonPress() {
-        this.props.removeItems();
+    onButtonPress(name) {
+        var that = this;
+        var getData
+        var deletedObject
+        var starCountRef = firebase.database.ref('users/MyItems').orderByChild('name').equalTo(name).once('value', function (snapshot) {
+            //     var starCountRef = firebase.database.ref('users/' + currentUser.uid + '/Cart').orderByChild('id').equalTo(id).once('value', function (snapshot) {
+            console.log("deleted VAlue", snapshot.val());
+            getData = snapshot.val()
+            for (let [key, value] of Object.entries(getData)) {
+                console.log("got Object", key, value);
+                deletedObject = key
+                console.log("deletedObject", deletedObject);
+            }
+
+
+        });
+
+        this.props.removeItems(deletedObject);
     }
     render() {
         var arrayObject = []
@@ -36,22 +53,32 @@ class VeiwItems extends Component {
                         arrayObject.map((item, i) => {
                             return (
                                 <ScrollView>
-                                    <Card key={i} style={{ flex: 1 }}>
-                                        <CardItem >
+                                    <Card key={i} style={{ flex: 1, backgroundColor: '#E3F2FD', }}>
+                                        <CardItem style={{ flex: 1, backgroundColor: '#E3F2FD', }}>
                                             <List >
                                                 <ListItem style={styles.list} >
-                                                    <Text style={{ color: '#88FF00' }} >Name:</Text><Text>{item.name}</Text>
+                                                    <Text style={{ color: 'black' }} >
+                                                        Name:   {item.name}
+                                                    </Text>
                                                 </ListItem>
                                                 <ListItem style={styles.list}>
-                                                    <Text style={{ color: '#88FF00' }}>Place:</Text><Text>{item.place}</Text>
+                                                    <Text style={{ color: 'black' }} >
+                                                        Place:   {item.place}
+                                                    </Text>
                                                 </ListItem>
                                                 <ListItem style={styles.list}>
-                                                    <Text style={{ color: '#88FF00' }}>Date:</Text><Text>{item.date}</Text>
+                                                    <Text style={{ color: 'black' }} >
+                                                        Date:   {item.date}
+                                                    </Text>
                                                 </ListItem>
+
+                                                <Icon style={{ marginLeft: 320, color: 'red' }} name='trash' onPress={() => this.onButtonPress(item.name)} />
                                             </List>
-                                            <Button block success onPress={this.onButtonPress.bind(this)}><Text> Remove Item</Text></Button>
+
                                         </CardItem>
+
                                     </Card>
+
 
                                 </ScrollView>
                             )
@@ -59,26 +86,38 @@ class VeiwItems extends Component {
                     }
 
                 </Content>
-            </Container>
+                <Fab
+                    containerStyle={{}}
+                    style={{ backgroundColor: '#5067FF' }}
+                    position="bottomRight"
+                    onPress={() => Actions.addItem()}
+                >
+                    <Icon name="add" />
+                </Fab>
+            </Container >
         );
     }
 };
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#E3F2FD',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        marginTop: -10
     },
     cardItem: {
         flex: 1,
         width: 350,
-        height: 'auto'
+        height: 'auto',
+        backgroundColor: '#E3F2FD',
     },
     list: {
         width: 350,
-        padding: 15,
-        color: '#88FF00'
+        height: 20,
+        padding: 5,
+        backgroundColor: '#E3F2FD',
+
     }
 
 });
