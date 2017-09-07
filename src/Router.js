faa0fd62cb6275b8ebed50853df1041d5f65c1b0
farooqmajeed <farooq306@hotmail.com>
2017-09-07 13:15:17 +0500

    Routing

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,6 @@ import { View, ScrollView } from 'react-native';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components//Auth/LoginForm';
 import SignupForm from './components/Auth/SignUp';
-import Dashboard from './components/dashboard';
 import AddItem from './components/AppComponents/AddItem'
 import VeiwItems from './components/AppComponents/VeiwItem'
 
@@ -12,19 +11,14 @@ const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 65 }}>
 
-            {/* <Scene key="auth">
+            <Scene key="auth">
                 <Scene key="login" component={LoginForm} hideNavBar={true} initial />
                 <Scene key="signup" component={SignupForm} hideNavBar={true} />
-            </Scene> */}
-
-
-            <Scene key="main">
-                <Scene key="dashboard" component={Dashboard} hideNavBar={true} />
-                <Scene navigationBarStyle={{ backgroundColor: '#4BB543' }} key="addItem" component={AddItem} title={"Add Item"}  />
-                <Scene navigationBarStyle={{ backgroundColor: '#4BB543' }} key="viewItem" component={VeiwItems} title={"View Item"} initial />
-
             </Scene>
-
+            <Scene key="main">
+                <Scene navigationBarStyle={{ backgroundColor: '#2196F3' }} key="addItem" component={AddItem} title={"Add Item"} />
+                <Scene navigationBarStyle={{ backgroundColor: '#2196F3' }} key="viewItem" component={VeiwItems} title={"View Items"} initial />
+            </Scene>
         </Router>
     );
 };
